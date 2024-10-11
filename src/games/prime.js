fc7032a47ec8729bffb1686553e868c022a75616
Kumiho <enotmos@yandex.ru>
2024-10-11 16:53:52 +0300

    fixed src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,27 +1,16 @@
-import getRandomInt from '../randomint.js';
-import brainGameStructure from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+import isPrime from '../isPrime.js';
+import runBrainGameStructure from '../index.js';
 
 const playBrainPrimeGame = () => {
-  const isPrime = (num) => {
-    if (num < 2) {
-      return false;
-    }
-    for (let i = 2; i <= Math.floor(num / 2); i += 1) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  const brainPrimeGameRound = () => {
-    const randomInt = getRandomInt(1, 100);
-    const correctAnswer = isPrime(randomInt) ? 'yes' : 'no';
-    return [randomInt, correctAnswer];
+  const runBrainPrimeGameRound = () => {
+    const randomNumber = getRandomNumber(1, 100);
+    const answer = isPrime(randomNumber) ? 'yes' : 'no';
+    return [randomNumber, answer];
   };
 
   const rules = 'Answer "yes" if the number is prime, otherwise answer "no".';
-  brainGameStructure(rules, brainPrimeGameRound);
+  runBrainGameStructure(rules, runBrainPrimeGameRound);
 };
 
 export default playBrainPrimeGame;
