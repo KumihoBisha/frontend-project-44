0b232462c86d6d8aaa6a56b5382a963188ed9766
Kumiho <enotmos@yandex.ru>
2024-10-11 16:54:12 +0300

    fixed src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,36 +1,24 @@
-import getRandomInt from '../randomint.js';
-import brainGameStructure from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+import generateProgression from '../generateProgression.js';
+import runBrainGameStructure from '../index.js';
 
 const playBrainProgressionGame = () => {
-  const makeProgression = () => {
-    const progression = [];
-    const stepProgression = getRandomInt(1, 10);
-    const startProgression = getRandomInt(1, 9);
-    let step = 0;
-    for (let i = 0; i < 10; i += 1) {
-      progression.push(startProgression + step);
-      step += stepProgression;
-    }
-    return progression;
-  };
+  const runBrainProgressionGameRound = () => {
+    const progressionStart = getRandomNumber(1, 9);
+    const progressionStep = getRandomNumber(1, 10);
+    const progressionLength = 10;
 
-  const hideElement = (progression) => {
-    const hiddenElementIndex = getRandomInt(0, progression.length - 1);
-    const hiddenElement = progression[hiddenElementIndex];
-    const progressionWithHiddenElement = progression.slice();
-    progressionWithHiddenElement[hiddenElementIndex] = '..';
-    return [progressionWithHiddenElement, hiddenElement];
-  };
+    const progression = generateProgression(progressionStart, progressionStep, progressionLength);
+    const hiddenElementIndex = getRandomNumber(0, progression.length - 1);
 
-  const brainProgressionGameRound = () => {
-    const [progression, hiddenElement] = hideElement(makeProgression());
+    const answer = String(progression[hiddenElementIndex]);
+    progression[hiddenElementIndex] = '..';
     const question = progression.join(' ');
-    const correctAnswer = hiddenElement;
-    return [question, correctAnswer];
+    return [question, answer];
   };
 
   const rules = 'What number is missing in the progression?';
-  brainGameStructure(rules, brainProgressionGameRound);
+  runBrainGameStructure(rules, runBrainProgressionGameRound);
 };
 
 export default playBrainProgressionGame;
