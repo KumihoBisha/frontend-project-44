c016af568772c14989044b88f12eb37fad93145b
Kumiho <enotmos@yandex.ru>
2024-10-16 13:11:07 +0300

    deleted irrelevant files

--- src/getGreatestCommonDivisor.js
+++
@@ -1,9 +0,0 @@
-const getGreatestCommonDivisor = (number1, number2) => {
-  if (number2 !== 0) {
-    const remainder = number1 % number2;
-    return getGreatestCommonDivisor(number2, remainder);
-  }
-  return number1;
-};
-
-export default getGreatestCommonDivisor;
