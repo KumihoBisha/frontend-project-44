d26a363444c1c566d1d2bbf237695b634c490a01
Kumiho <enotmos@yandex.ru>
2024-10-16 13:13:52 +0300

    fixed src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,19 +1,26 @@
 import getRandomNumber from '../getRandomNumber.js';
-import getGreatestCommonDivisor from '../getGreatestCommonDivisor.js';
 import runBrainGameStructure from '../index.js';
 
+const getGreatestCommonDivisor = (number1, number2) => {
+  if (number2 !== 0) {
+    const remainder = number1 % number2;
+    return getGreatestCommonDivisor(number2, remainder);
+  }
+  return number1;
+};
+
+const runBrainGcdGameRound = () => {
+  const number1 = getRandomNumber(1, 99);
+  const number2 = getRandomNumber(1, 99);
+
+  const question = `${number1} ${number2}`;
+  const answer = String(getGreatestCommonDivisor(number1, number2));
+  return [question, answer];
+};
+
 const playBrainGcdGame = () => {
-  const runBrainGcdGameRound = () => {
-    const number1 = getRandomNumber(1, 99);
-    const number2 = getRandomNumber(1, 99);
-
-    const question = `${number1} ${number2}`;
-    const answer = String(getGreatestCommonDivisor(number1, number2));
-    return [question, answer];
-  };
-
-  const rules = 'Find the greatest common divisor of given numbers.';
-  runBrainGameStructure(rules, runBrainGcdGameRound);
+  const description = 'Find the greatest common divisor of given numbers.';
+  runBrainGameStructure(description, runBrainGcdGameRound);
 };
 
 export default playBrainGcdGame;
