64400b2bce699358a726a1a884ce5e271ddaa308
Kumiho <enotmos@yandex.ru>
2024-10-16 13:13:32 +0300

    fixed src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,17 @@
 import getRandomNumber from '../getRandomNumber.js';
-import isEven from '../isEven.js';
 import runBrainGameStructure from '../index.js';
 
-const playBrainEvenGame = () => {
-  const runBrainEvenGameRound = () => {
-    const randomNumber = getRandomNumber(1, 100);
-    const answer = isEven(randomNumber) ? 'yes' : 'no';
-    return [randomNumber, answer];
-  };
+const isEven = (num) => num % 2 === 0;
 
-  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-  runBrainGameStructure(rules, runBrainEvenGameRound);
+const runBrainEvenGameRound = () => {
+  const randomNumber = getRandomNumber(1, 100);
+  const answer = isEven(randomNumber) ? 'yes' : 'no';
+  return [randomNumber, answer];
+};
+
+const playBrainEvenGame = () => {
+  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+  runBrainGameStructure(description, runBrainEvenGameRound);
 };
 
 export default playBrainEvenGame;
