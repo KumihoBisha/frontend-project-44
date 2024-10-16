cd98350a63fd34e8537cc02280b36846a3687931
Kumiho <enotmos@yandex.ru>
2024-10-16 13:13:06 +0300

    fixed src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,23 +1,35 @@
 import getRandomNumber from '../getRandomNumber.js';
-import getValueOfExpression from '../getValueOfExpression.js';
 import runBrainGameStructure from '../index.js';
 
+const calculateExpression = (operator, number1, number2) => {
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      throw new Error(`Wrong operator ${operator}`);
+  }
+};
+
+const runBrainCalcGameRound = () => {
+  const operators = ['+', '-', '*'];
+  const operatorIndex = getRandomNumber(0, operators.length - 1);
+  const operator = operators[operatorIndex];
+
+  const number1 = getRandomNumber(1, 99);
+  const number2 = getRandomNumber(1, 99);
+
+  const question = `${number1} ${operator} ${number2}`;
+  const answer = String(calculateExpression(operator, number1, number2));
+  return [question, answer];
+};
+
 const playBrainCalcGame = () => {
-  const runBrainCalcGameRound = () => {
-    const operators = ['+', '-', '*'];
-    const operatorIndex = getRandomNumber(0, operators.length - 1);
-    const operator = operators[operatorIndex];
-
-    const number1 = getRandomNumber(1, 99);
-    const number2 = getRandomNumber(1, 99);
-
-    const question = `${number1} ${operator} ${number2}`;
-    const answer = String(getValueOfExpression(operator, number1, number2));
-    return [question, answer];
-  };
-
-  const rules = 'What is the result of the expression?';
-  runBrainGameStructure(rules, runBrainCalcGameRound);
+  const description = 'What is the result of the expression?';
+  runBrainGameStructure(description, runBrainCalcGameRound);
 };
 
 export default playBrainCalcGame;
